913ce96934b07d2c49da7a7b692e864535cd5279
max312444 <max12max@g.yju.ac.kr>
2025-11-03 23:42:41 +0900

    Healthcheck 수정 7

--- grpc_service/service/healthcheck_service.rb
+++ grpc_service/service/healthcheck_service.rb
@@ -8,18 +8,8 @@ module Bannote
   module CommonService
     module Healthcheck
       class HealthServiceHandler < ::Grpc::Health::V1::Health::Service
-        # 단일 요청 헬스체크
         def check(request, _call)
-          puts "[HealthCheck] Service: #{request.service}"
-          ::Grpc::Health::V1::HealthCheckResponse.new(status: :SERVING)
-        end
-
-        # 스트리밍 요청 헬스체크 (옵션)
-        def watch(request, call)
-          loop do
-            call.send_msg(::Grpc::Health::V1::HealthCheckResponse.new(status: :SERVING))
-            sleep 10
-          end
+          ::Grpc::Health::V1::HealthCheckResponse.new(status: "SERVING")
         end
       end
     end
