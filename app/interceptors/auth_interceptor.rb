e944f5221dea33bf4fb34a1b7943655870a22183
max312444 <max12max@g.yju.ac.kr>
2025-11-25 15:59:17 +0900

    권한 배열로 받을 수 있게 수정 2차

--- app/interceptors/auth_interceptor.rb
+++ app/interceptors/auth_interceptor.rb
@@ -2,7 +2,7 @@
 
 require 'grpc'
 require_relative '../../app/models/concerns/current'
-require_relative '../../grpc_service/common/role_priority'   # 추가
+require_relative '../../app/models/concerns/simulated_user_roles'   # 지금은 목데이터 사용
 
 class AuthInterceptor < GRPC::ServerInterceptor
   UNAUTHENTICATED = GRPC::BadStatus.new(
@@ -11,28 +11,35 @@ class AuthInterceptor < GRPC::ServerInterceptor
   )
 
   def request_response(request:, call:, method:)
+    # HealthCheck는 인증 제외
     if method.to_s.match?(/Health|health/i)
-      puts "[AuthInterceptor] Skipping authentication for HealthCheck method: #{method}"
       return yield
     end
 
+    # 필수: 유저 코드
     user_code = call.metadata['x-user-code']
-    user_role = (call.metadata['x-user-role'] || "student").to_s.downcase
-
-    roles = user_role.split(',').map { |r| r.strip }
+    roles_header = (call.metadata['x-user-role'] || "student").to_s.downcase
 
     if user_code.nil? || user_code.empty?
       raise UNAUTHENTICATED
     end
 
-    # 받은 값들 중 높은 권한 찾는 로직
-    highest_role = RolePriority.highest(roles)
+    # 권한이 여러 개 들어올 수 있으므로 배열로 변환
+    roles = roles_header.split(',').map(&:strip)
+    # 목데이터 사용으로 아래 코드 사용 유저 서비스 연결 시 주석처리 or 삭제
+    highest_role = roles.max_by { |r| SimulatedUserRoles::AUTHORITY_LEVELS[r] || 0 }
 
-    # 가장 높은 권한으로 적용
+    # 권한이 enum이 들어올 수도 있음 → 숫자로 변환 후 매핑 해야하나?
+    # 유저 서비스 연결시 아래 코드 사용
+    # roles_enum = roles_header.split(',').map(&:to_i) 
+    # roles = roles_enum.map { |v| USER_ROLE_MAP[v] }
+    # highest_role = roles.max_by { |r| PRIORITY[r] }
+
+    # 최종 적용
     Current.user_code = user_code
     Current.user_role = highest_role
 
-    puts "[AuthInterceptor] user_code=#{user_code}, raw_roles=#{roles}, highest_role=#{highest_role}"
+    puts "[Auth] user_code=#{user_code}, roles=#{roles}, highest_role=#{highest_role}"
 
     yield
 
